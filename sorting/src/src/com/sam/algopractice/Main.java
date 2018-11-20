3ff0add218d87495988baa4af6096dd1d1b69c4b
Soumalya Dey <Soumalya_Dey@epam.com>
2018-11-20 11:23:48 +0100

    Quick Sorting

---
+++ sorting/src/src/com/sam/algopractice/Main.java
@@ -0,0 +1,22 @@
+package src.com.sam.algopractice;
+
+import java.util.Arrays;
+import java.util.List;
+
+import com.sam.algopractice.QuickSort;
+
+public class Main {
+    private static QuickSort quickSort;
+
+    public static void main(String[] args) {
+        List<Integer> list = Arrays.asList(99, 10, 11, 78, 35, 84, 37, 67, 52, 4, 66, 91, 83, 7, 0, 74, 125, 61, 40);
+        System.out.println(list);
+
+        //Quick Sort
+        quickSort = new QuickSort();
+        quickSort.quickSort(list);
+        System.out.println(list);
+
+        //Radix Sort
+    }
+}
